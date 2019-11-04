e7c281dc45d15379129cce343f0912b8f299e8f7
Karina Macancela <karymacancela@gmail.com>
2019-11-04 10:10:57 -0500

    Adding search and form functionalities

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -3,10 +3,24 @@ import PokemonCard from './PokemonCard'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCollection extends React.Component {
+
+  findHP = (pokemon) => {
+    let hp = pokemon.stats.filter(stat => stat["name"] === "hp")
+    return hp[0]["value"]
+  }
+
+  filterPokemons = () => {
+    return this.props.pokemons.filter(pokemon =>
+      pokemon.name.includes(this.props.filter)
+    )
+  }
+
   render() {
     return (
       <Card.Group itemsPerRow={6}>
-        <h1>Hello From Pokemon Collection</h1>
+        { this.filterPokemons().map(pokemon => {
+          return <PokemonCard name={ pokemon.name } img={ pokemon.sprites } hp={this.findHP(pokemon)}/>
+        }) }
       </Card.Group>
     )
   }
--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -13,11 +13,37 @@ class PokemonForm extends React.Component {
     }
   }
 
+  handleSubmit = (event) => {
+    event.preventDefault()
+    fetch("http://localhost:3000/pokemon", {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "name": event.target.name.value,
+        "stats": [{
+          "value": event.target.hp.value,
+          "name": "hp"
+        }],
+        "sprites": {
+          "front": event.target.frontUrl.value,
+          "back": event.target.backUrl.value
+        }
+      })
+    })
+    .then(r => r.json())
+    .then(data => {
+      this.props.addPokemon(data)
+    })
+  }
+
   render() {
     return (
       <div>
         <h3>Add a Pokemon!</h3>
-        <Form onSubmit={this.handleSubmit}>
+        <Form onSubmit={ this.handleSubmit }>
           <Form.Group widths="equal">
             <Form.Input fluid label="Name" placeholder="Name" name="name" />
             <Form.Input fluid label="hp" placeholder="hp" name="hp" />
